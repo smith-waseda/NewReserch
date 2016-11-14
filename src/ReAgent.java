785c3808e52057970c8670f9f519e8b2921d48a5
s.nakamura <dropanddrop@moegi.waseda.jp>
2016-11-14 21:05:22 +0900

    新要素の追加

---
+++ src/ReAgent.java
@@ -0,0 +1,13 @@
+/**
+ * Created by s.nakamura on 2016/11/14.
+ */
+public class ReAgent extends Agent {
+    public boolean express;
+    public double opinion;
+    public int number;
+    public int layer;
+
+    ReAgent(){
+        super(0,0);
+    }
+}
